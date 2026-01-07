efa2175c462f5c2fbdc287906fb156a15bf6ed85
Adriana Geamanu <adrianageamanu24@gmail.com>
2026-01-07 23:02:29 +0200

    implemented load audio function

--- core/audio_loader.py
+++ core/audio_loader.py
@@ -7,3 +7,11 @@ def stereo_to_mono(stereo):
     mono = mono / np.max(np.abs(mono))
 
     return mono
+
+def load_audio(path):
+    audio, _ = librosa.load(path, sr=config.SAMPLE_RATE, mono=False)
+    
+    if audio.ndim == 2:
+        audio = stereo_to_mono(audio)
+    
+    return audio
